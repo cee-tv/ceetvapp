97708d792e72d6986761444d526d9d0289d8a4c7
Jayce <kendump666@gmail.com>
2024-07-12 15:18:20 +0800

    Update stacktv.js

--- js/stacktv.js
+++ js/stacktv.js
@@ -219,7 +219,7 @@
 			let switchs = document.createElement("div");
 			let switchlist = document.createElement("div");
 			let switchHead = document.createElement("div");
-			switchHead.innerText = "节目单";
+			switchHead.innerText = "Channel list";
 			let switchSearch = document.createElement("div");
 			let input = document.createElement("input");
 			input.placeholder = "搜索节目";
